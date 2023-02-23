21224c001de236a72d79d2d2aead6af090b8e630
Georgia Proctor <georgiaproctors@gmail.com>
2023-02-23 12:26:07 +0000

    validations and associations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates_uniqueness_of :movie_id, scope: :list_id
+  validates :comment, length: { minimum: 6 }
+end
